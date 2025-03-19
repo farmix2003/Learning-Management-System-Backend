8581ad4589e4a06bcc8df890daeda8c2ba347c0e
Farmix2003 <ffarrux386@gmail.com>
2025-03-19 19:11:35 +0200

    Repositories creation

---
+++ src/main/java/farmix/com/repository/CourseRepository.java
@@ -0,0 +1,9 @@
+package farmix.com.repository;
+
+import farmix.com.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+}
